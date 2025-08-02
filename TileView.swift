2286752cdba8d6f8f32959d2bec1a4bc562b0a7f
mizech <michael.zech@hotmail.de>
2025-08-02 15:31:12 +0200

    Until 15'

---
+++ TileView.swift
@@ -0,0 +1,18 @@
+//
+//  TileView.swift
+//  PhotoFinish
+//
+//  Created by Michael on 02.08.25.
+//
+
+import SwiftUI
+
+struct TileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TileView()
+}
