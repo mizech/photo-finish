60466eff509aea149dc9727a92c6df8dee3aa06f
mizech <michael.zech@hotmail.de>
2025-08-02 15:03:50 +0200

    Initial Commit

---
+++ PhotoFinish/PhotoFinishApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFinishApp.swift
+//  PhotoFinish
+//
+//  Created by Michael on 02.08.25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFinishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
